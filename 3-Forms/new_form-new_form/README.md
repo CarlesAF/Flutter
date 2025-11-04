b1c4774786eafa01c25c9e74cc607758448fa952
Carles <caragfer@gmail.com>
2025-11-04 22:42:02 +0100

    formulario 1 showdialogs acabado + readme

--- 3-Forms/new_form-new_form/README.md
+++ 3-Forms/new_form-new_form/README.md
@@ -1,16 +1,3 @@
-# new_form
+# Videos of the Both apps 'Cervatell' 'PersonalApp'
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+*link* https://drive.google.com/drive/folders/156D-vjHWH8jaSOsZk9o2ZhLGRlLsrJ0J?usp=sharing 
--- 3-Forms/new_form-new_form/lib/main.dart
+++ 3-Forms/new_form-new_form/lib/main.dart
@@ -37,42 +37,118 @@ class _MyCustomFormState extends State<MyCustomForm> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: TextField(
-          controller: myController,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-              backgroundColor: Colors.transparent,
-              context: context,
-              builder: (BuildContext context) {
-                return Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.amber,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(25),
-                        topRight: Radius.circular(25),
+        child: Column(
+          children: [
+            TextField(
+              controller: myController,
+            ),
+            Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 255, 244, 229),
+                        foregroundColor: Colors.black87,
+                        side: const BorderSide(color: Color.fromARGB(255, 255, 237, 209)),
                       ),
-                    ),
-                    height: 200,
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text(myController.text),
-                          ElevatedButton(
-                            onPressed: () => Navigator.of(context).pop(),
-                            child: const Text('Tancar BottomSheet'),
-                          )
+                      onPressed: () {
+                      showDialog(
+                      context: context,
+                      barrierDismissible: true,
+                      builder: (BuildContext context) {
+                        return SimpleDialog(
+                        title: const Text('SimpleDialog'),
+                        children:[
+                          Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Text(myController.text),
+                          ),
                         ],
-                      ),
-                    ));
-              });
-        },
-        tooltip: 'Mostra el valor!',
-        child: const Icon(Icons.text_fields),
+                        );
+                      },
+                      );
+                }, child: const Text('simpleDialog'),
+              ),
+              OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 229, 255, 242),
+                    foregroundColor: Colors.black87,
+                    side: const BorderSide(color: Color.fromARGB(255, 171, 207, 196)),
+                ),
+                onPressed: () {
+                showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text('Warning'),
+                      content: Text(myController.text),
+                      actions: [
+                        ElevatedButton(
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: const Text('Tancar'),
+                        )
+                      ]);
+                  } 
+                  );
+              }, child: const Text('alertDialog'),
+              ),
+
+              OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 229, 235, 255), 
+                foregroundColor: Colors.black87,
+                side: const BorderSide(color: Color.fromARGB(255, 209, 225, 255)),
+                ),
+                onPressed: (){
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Això és un SnackBar'),
+                    duration: Duration(seconds: 2),
+                ));
+              }, child: const Text( 'snackBar'),
+              ),
+              OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 253, 229, 255), 
+                  foregroundColor: Colors.black87,
+                  side: const BorderSide(color: Color.fromARGB(255, 239, 201, 247)),),
+                onPressed: () {
+                  showModalBottomSheet(
+                      backgroundColor: Colors.transparent,
+                      context: context,
+                      builder: (BuildContext context) {
+                        return Container(
+                            decoration: const BoxDecoration(
+                              color: Colors.amber,
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(25),
+                                topRight: Radius.circular(25),
+                              ),
+                            ),
+                            height: 200,
+                            child: Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  Text(myController.text),
+                                  ElevatedButton(
+                                    onPressed: () => Navigator.of(context).pop(),
+                                    child: const Text('Tancar BottomSheet'),
+                                  )
+                                ],
+                              ),
+                            ));
+                      });
+                }, child: const Text('bottomSheet'),
+              ),
+              ]
+            ) 
+        )],
+        ),
       ),
     );
   }
