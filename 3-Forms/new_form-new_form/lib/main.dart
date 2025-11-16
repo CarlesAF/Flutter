336bd1559c9ab9dd192ba3a28917b76f29676047
Carles <caragfer@gmail.com>
2025-11-16 18:20:48 +0100

    flutter format .

--- 3-Forms/new_form-new_form/lib/main.dart
+++ 3-Forms/new_form-new_form/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Recuperar el valor d''un camp de text',
+      title: 'Recuperar el valor d' 'un camp de text',
       home: MyCustomForm(),
     );
   }
@@ -44,110 +44,117 @@ class _MyCustomFormState extends State<MyCustomForm> {
             ),
             Expanded(
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 255, 244, 229),
-                        foregroundColor: Colors.black87,
-                        side: const BorderSide(color: Color.fromARGB(255, 255, 237, 209)),
-                      ),
-                      onPressed: () {
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 255, 244, 229),
+                      foregroundColor: Colors.black87,
+                      side: const BorderSide(
+                          color: Color.fromARGB(255, 255, 237, 209)),
+                    ),
+                    onPressed: () {
                       showDialog(
-                      context: context,
-                      barrierDismissible: true,
-                      builder: (BuildContext context) {
-                        return SimpleDialog(
-                        title: const Text('SimpleDialog'),
-                        children:[
-                          Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Text(myController.text),
-                          ),
-                        ],
-                        );
-                      },
+                        context: context,
+                        barrierDismissible: true,
+                        builder: (BuildContext context) {
+                          return SimpleDialog(
+                            title: const Text('SimpleDialog'),
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Text(myController.text),
+                              ),
+                            ],
+                          );
+                        },
                       );
-                }, child: const Text('simpleDialog'),
-              ),
-              OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 229, 255, 242),
-                    foregroundColor: Colors.black87,
-                    side: const BorderSide(color: Color.fromARGB(255, 171, 207, 196)),
-                ),
-                onPressed: () {
-                showDialog(
-                  context: context,
-                  barrierDismissible: false,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: const Text('Warning'),
-                      content: Text(myController.text),
-                      actions: [
-                        ElevatedButton(
-                          onPressed: () => Navigator.of(context).pop(),
-                          child: const Text('Tancar'),
-                        )
-                      ]);
-                  } 
-                  );
-              }, child: const Text('alertDialog'),
-              ),
-
-              OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 229, 235, 255), 
-                foregroundColor: Colors.black87,
-                side: const BorderSide(color: Color.fromARGB(255, 209, 225, 255)),
-                ),
-                onPressed: (){
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text('Això és un SnackBar'),
-                    duration: Duration(seconds: 2),
-                ));
-              }, child: const Text( 'snackBar'),
-              ),
-              OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 253, 229, 255), 
-                  foregroundColor: Colors.black87,
-                  side: const BorderSide(color: Color.fromARGB(255, 239, 201, 247)),),
-                onPressed: () {
-                  showModalBottomSheet(
-                      backgroundColor: Colors.transparent,
-                      context: context,
-                      builder: (BuildContext context) {
-                        return Container(
-                            decoration: const BoxDecoration(
-                              color: Colors.amber,
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(25),
-                                topRight: Radius.circular(25),
-                              ),
-                            ),
-                            height: 200,
-                            child: Center(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  Text(myController.text),
+                    },
+                    child: const Text('simpleDialog'),
+                  ),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 229, 255, 242),
+                      foregroundColor: Colors.black87,
+                      side: const BorderSide(
+                          color: Color.fromARGB(255, 171, 207, 196)),
+                    ),
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          barrierDismissible: false,
+                          builder: (context) {
+                            return AlertDialog(
+                                title: const Text('Warning'),
+                                content: Text(myController.text),
+                                actions: [
                                   ElevatedButton(
-                                    onPressed: () => Navigator.of(context).pop(),
-                                    child: const Text('Tancar BottomSheet'),
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(),
+                                    child: const Text('Tancar'),
                                   )
-                                ],
-                              ),
-                            ));
-                      });
-                }, child: const Text('bottomSheet'),
-              ),
-              ]
-            ) 
-        )],
+                                ]);
+                          });
+                    },
+                    child: const Text('alertDialog'),
+                  ),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 229, 235, 255),
+                      foregroundColor: Colors.black87,
+                      side: const BorderSide(
+                          color: Color.fromARGB(255, 209, 225, 255)),
+                    ),
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text('Això és un SnackBar'),
+                        duration: Duration(seconds: 2),
+                      ));
+                    },
+                    child: const Text('snackBar'),
+                  ),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 253, 229, 255),
+                      foregroundColor: Colors.black87,
+                      side: const BorderSide(
+                          color: Color.fromARGB(255, 239, 201, 247)),
+                    ),
+                    onPressed: () {
+                      showModalBottomSheet(
+                          backgroundColor: Colors.transparent,
+                          context: context,
+                          builder: (BuildContext context) {
+                            return Container(
+                                decoration: const BoxDecoration(
+                                  color: Colors.amber,
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(25),
+                                    topRight: Radius.circular(25),
+                                  ),
+                                ),
+                                height: 200,
+                                child: Center(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: <Widget>[
+                                      Text(myController.text),
+                                      ElevatedButton(
+                                        onPressed: () =>
+                                            Navigator.of(context).pop(),
+                                        child: const Text('Tancar BottomSheet'),
+                                      )
+                                    ],
+                                  ),
+                                ));
+                          });
+                    },
+                    child: const Text('bottomSheet'),
+                  ),
+                ]))
+          ],
         ),
       ),
     );
