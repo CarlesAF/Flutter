31c6a3ac2765f963be8eeffae0b4a0effa1c05e0
CarlesAF <caragfer@gmail.com>
2025-12-15 17:01:51 +0100

    clonacion del proyecto + cambio a mi api key

---
+++ 4-Api/movies2526/lib/api/api.dart
@@ -0,0 +1,5 @@
+class Api {
+  static const baseUrl = "https://api.themoviedb.org/3/";
+  static const imageBaseUrl = "https://image.tmdb.org/t/p/original/";
+  static const apiKey = "df4c8eb65c8c23eb8879727f62f83a9c";
+}
